2eeafd0f8fbc02974146c1a920e8264bddd596d7
Tegan Snyder <tsnyder@tegdesign.com>
2013-11-29 16:45:04 -0600

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE magento;
+CREATE USER 'magento'@'localhost' IDENTIFIED BY 'changeme';
+GRANT ALL PRIVILEGES ON *.* TO 'magento'@'localhost' WITH GRANT OPTION;
+CREATE USER 'magento'@'%' IDENTIFIED BY 'changeme';
+GRANT ALL PRIVILEGES ON *.* TO 'magento'@'%' WITH GRANT OPTION;
