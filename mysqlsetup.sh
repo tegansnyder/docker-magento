9cdf349d7bb3eb7524b1e0aa2e060df683d22217
Tegan Snyder <tsnyder@tegdesign.com>
2013-11-29 18:35:17 -0600

    Update mysqlsetup.sh

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+
+# Keep upstart from complaining
+RUN dpkg-divert --local --rename --add /sbin/initctl
+RUN ln -s /bin/true /sbin/initctl
+
+sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+/usr/sbin/mysqld &
+sleep 5
+echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'mysql-server' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
 CREATE DATABASE magento;
 CREATE USER 'magento'@'localhost' IDENTIFIED BY 'changeme';
 GRANT ALL PRIVILEGES ON *.* TO 'magento'@'localhost' WITH GRANT OPTION;
