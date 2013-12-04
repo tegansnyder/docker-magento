fd2a3718d4269080e57e4f04a18abb345da5bef8
Tegan Snyder <tsnyder@tegdesign.com>
2013-12-04 14:05:51 -0600

    Update mysqlsetup.sh

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/bin/mysqld_safe &
-sleep 5
+sleep 10
 /usr/bin/mysql -u root -e "CREATE DATABASE magento; CREATE USER 'magento'@'localhost' IDENTIFIED BY 'changeme'; GRANT ALL PRIVILEGES ON *.* TO 'magento'@'localhost' WITH GRANT OPTION; CREATE USER 'magento'@'%' IDENTIFIED BY 'changeme'; GRANT ALL PRIVILEGES ON *.* TO 'magento'@'%' WITH GRANT OPTION;"
